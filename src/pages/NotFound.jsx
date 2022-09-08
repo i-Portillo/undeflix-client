f1772931b857f50a0bc983dd15b60e483f1a8230
iPortillo <ismaelpa91@gmail.com>
2022-09-08 17:23:38 +0200

    Proper implementation of Not Found page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,17 @@
+import { Box, Paper, Typography } from '@mui/material'
 import React from 'react'
 
 export default function NotFound() {
   return (
-    <div>NotFound</div>
+    <Paper sx={{ maxWidth: 'sm', p: 4, m: 'auto', mt: 11, backgroundColor: 'primary.main', height: '100%', maxHeight: '750px'}} >
+      <Box mb={2} >
+        <Typography color='secondary.dark' variant='h1' mb={4} >
+          404 ERROR
+        </Typography>
+        <Typography color='secondary.dark' variant='h4' >
+          Webpages? Where we're going, we don't need webpages.
+        </Typography>
+      </Box>
+    </Paper>
   )
 }
